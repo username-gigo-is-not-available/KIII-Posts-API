e394e571d0c1876f3e236353871e4c67903655ed
1212-1212 <57277647+1212-1212@users.noreply.github.com>
2023-06-30 23:11:14 +0200

    Update K8s: Add DB Service and default namespace script

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,1 +1,11 @@
-kubectl apply -f namespace.yaml -f  posts-service-config-map.yaml -f posts-service-secrets.yaml -f deployment.yaml -f ingress.yaml -f service.yaml -f posts-service-mongodb-config-map.yaml -f posts-service-mongodb-secrets.yaml -f posts-service-mongodb-persistent-volume-claim.yaml -f stateful-set.yaml
+kubectl apply -f namespace.yaml \
+-f  posts-service-config-map.yaml \
+-f posts-service-secrets.yaml \
+-f deployment.yaml \
+-f ingress.yaml \
+-f posts-service-service.yaml \
+-f posts-service-mongodb-config-map.yaml \
+-f posts-service-mongodb-secrets.yaml \
+-f posts-service-mongodb-persistent-volume-claim.yaml \
+-f posts-service-mongodb-service.yaml \
+-f stateful-set.yaml
